d638a954bdbcf3897884c48c5384ce38f1e11b03
AYRtonMeD <ayrtondantas2013@gmail.com>
2025-08-25 19:16:26 -0300

    feat: cria projeto com vite

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import BarChart from './components/BarChart'
+
+export default function App() {
+
+  return (
+    <>
+      <div style={{ height: '400px' }}>
+        <BarChart />
+      </div>
+    </>
+  )
+}
